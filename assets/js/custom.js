75b7484b7e9e289b08d81b216677c66f110f800e
Rajib Sarker <redhat0593@gmail.com>
2023-05-26 21:51:03 +0600

    u

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,19 @@
 AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
